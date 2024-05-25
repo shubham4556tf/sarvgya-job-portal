0d5d578b60afc4fe71abe26bc1f5ed0fbd9e8e4c
shubham4556tf <150424128+shubham4556tf@users.noreply.github.com>
2024-05-25 12:33:07 +0530

    add a local host to getting resutl on webpage

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.route("/")
+@app.route("/")
 def hello_world():
     return "<p>this is the test funtion</p>"
+if __name__=="__main__":
+    
+     app.run(host='0.0.0.0',port=50100,debug=True)
